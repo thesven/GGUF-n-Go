e512edf73855426d3d1ad8288abdce4fb624d4a0
Michael Svendsen <michael@mikesvendsen.net>
2024-05-24 22:08:51 -0400

    new coinfig files moved config files to their own dir

--- setup.sh
+++ setup.sh
@@ -17,5 +17,7 @@ rm -rf wikitext-2-raw-v1.zip
 # download the groups merged dataset
 curl -L -O https://github.com/ggerganov/llama.cpp/files/14194570/groups_merged.txt
 
+curl -L -O https://gist.githubusercontent.com/bartowski1182/b6ac44691e994344625687afe3263b3a/raw/d53a2c532e318ebb8258bb1ccb94ddb870b04be2/calibration_data.txt
+
 # create a directory to hold all of the models
 mkdir models
